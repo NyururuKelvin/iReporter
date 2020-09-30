c90339d0c6f1b5a51074dac82980cdc68671ce77
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-30 11:18:14 +0300

    my commit add code

---
+++ app/main/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, BooleanField
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError, Email, Required
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
+
+class PostForm(FlaskForm):
+    Type = SelectField('Category', choices=[('Red flag', 'Red flag'), ('Intervention', 'Intervention')], validators=[InputRequired(message="Type required")])
+    Title = StringField('Title', validators=[InputRequired(message="Title required")])
+    Description = StringField('Description', validators=[InputRequired(message="Description required")])
+    submit= SubmitField('Submit')
+
