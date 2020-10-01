0a1b263a3fc97b9d575e09846d7cf4e1d3dc3000
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-01 11:05:02 +0300

    my commit add model

--- app/models.py
+++ app/models.py
@@ -56,3 +56,22 @@ class Admin(db.Model):
 
     def __repr__(self):
         return f'Admin {self.username}'
+
+class Case(db.Model):
+    __tablename__ = "cases"
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(255)) # corruption or intervention
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255), index = True)
+    image = db.Column(db.String(255))
+    video = db.Column(db.String(255))
+    geolocation = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f"Case {self.description}"
+
+    @classmethod
+    def get_case(cls,id):
+        cases = Case.query.filter_by(id=id).all()
+        return cases
