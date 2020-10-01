cfece66cd4296165b8f72182f640e5f875a6df44
n-pretorious <pretoriousndungu@gmail.com>
2020-10-01 15:48:55 +0300

    update case form

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -4,10 +4,6 @@ from wtforms import SubmitField, SelectField
 from ..models import Status
 
 
-# def status_query():
-#   return Status.query
-
-
 class StatusForm(FlaskForm):
     status = SelectField(
         u"Status", choices=[(1, "Received"), (2, "Processing"), (3, "Rejected"), (4, "Resolved")]
