7ddc536ebcef90acefe8ab97677d22e332603260
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-02 01:07:37 +0300

    deploy

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_wtf import FlaskForm
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 # app = create_app('test')
 
 manager= Manager(app)
